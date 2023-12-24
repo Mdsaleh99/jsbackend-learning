490d0aa2d50034117de798043e7c5db8b89b5200
saleh <mdsaleh033@gmail.com>
2023-12-24 12:38:10 +0530

    Add video and user models

---
+++ src/models/user.model.js
@@ -0,0 +1,95 @@
+import mongoose, { Schema } from "mongoose";
+import jwt  from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+    {
+        username:{
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true   // searching field enable karna hai to index true karna padega aur ye index amazing topic hai
+        },
+        email:{
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+        fullname:{
+            type: String,
+            required: true,
+            index: true,
+            trim: true
+        },
+        avtar:{
+            type: String,   // cloudnary url
+            required: true,    
+        },
+        coverImage:{
+            type: String,   // cloudnary url    
+        },
+        watchHistory: [
+           {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+           }
+        ],
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+        },
+        refreshToken: {
+            type: String,
+        },
+        
+    },
+    { timestamps: true }
+)
+
+// pre save middleware and pre ek hook hai jo mongoose provide karta hai      https://mongoosejs.com/docs/7.x/docs/middleware.html#pre     pre hook me hume 2 parameter milte hai 1st parameter me hume ek string pass karna padega yani event pass karna hai (save ek event hai) aur 2nd parameter me hume ek function pass karna padega aur yah pe hume ek callback function pass karna padega lekin arrow function call back function nahi pass kar sakte hai kyu ki arrow function me this keyword ka use nahi kar sakte hai isliye hume normal function pass karna padega..... yah function async function hona chahiye kyu ki hume yah function async function banana padega kyu ki hume yah function me password ko hash karna hai aur password ko hash karne ke liye hume time lagta hai aur yah time hume async function me mil jayega....     next function ko hume call karna padega kyu ki yah next function hume next middleware ko call karne me help karega
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) return next()  // isModified ek mongoose method hai aur yah method hume yah batata hai ki password field me kuch change hua hai ya nahi
+    
+    this.password = bcrypt.hash(this.password, 10)  //10 is the salt round and salt round is the number of time the password is hashed 
+    next()
+} )
+
+// custom hooks b bana sakte hai mongoose me 
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+    jwt.sign(
+        {
+            _id: this._id,
+            username: this.username,
+            email: this.email,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY // expiry ko object me pass karna padega
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function () {
+    jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY // expiry ko object me pass karna padega
+        }
+    )
+}
+
+// jwt ek bearer token hai 
+
+
+export const User = mongoose.model("User", userSchema)
---
+++ src/models/viedo.model.js
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+
+const videoSchema = new Schema(
+    {
+        viedoFile: {
+            type: String,  // cloudnary url
+            required: true
+        },
+        thumbnail: {
+            type: String,  // cloudnary url
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        discription: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: Number, 
+            required: true
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+        },
+        owner: {
+          type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    }, { timestamps: true }
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)  // https://www.npmjs.com/package/mongoose-aggregate-paginate-v2 and mongoose option deta hai own plugin add karne ko o hai 'plugin'
+export const Video = mongoose.model("Video", videoSchema)
+
+// bcrypt is a password hashing function and it is used to hash the password
+// JWT is a JSON Web Token and it is used to create a token for the user  https://jwt.io/
